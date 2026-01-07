a886796b4b248f97c0c59c23c0ed4e82866cfa02
Nissi Fobe Neba <nissi.fobe.neba@stu.ocsb.ca>
2026-01-07 19:02:57 +0000

    buttons

--- code.py
+++ code.py
@@ -1,24 +1,57 @@
-#!/usr/bin/env python3
-
-# Created by: Mr. Coxall
-# Created on: July 2020
-# This program is the "Space Aliens" program on the PyBadge
-
 import ugame
 import stage
 
-
 def game_scene():
-    # this function is the main game game_scene
+    # This function is the main game scene
 
     # image banks for CircuitPython
-    image_bank_background = stage.Bank.from_bmp16(
-        "space_aliens_background.bmp")
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
-    # set the background to image 0 in the image bank
+    # sets the background to image in the image bank
     # and the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
 
-    # a sprite that will be updated every frame
     ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
+    # create a stage for the background to show up on
+    # and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [ship] + [background]
+
+    # render the background and initial location of sprite list
+
+    # most likely you will only render blocks
+    game.render_block()
+
+    # repeat forever, game loop
+    while True:
+
+        # get user input
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
+
+        #update game logic 
+        #redraw sprites 
+        game.render_sprites([ship])
+        game.tick()
+
+
+if __name__ == "__main__":
+    game_scene()
