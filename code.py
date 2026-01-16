4c574eacedf3a9562ea1adf169977358f2dd8f19
Nissi Fobe Neba <nissi.fobe.neba@stu.ocsb.ca>
2026-01-16 18:33:07 +0000

    lasers

--- code.py
+++ code.py
@@ -34,8 +34,8 @@ def game_scene():
     
     for  x_location in range(constants.SCREEN_GRID_X):
         for y_location in range(constants.SCREEN_GRID_Y):
-            title_picked=random.randint(1,3)
-            background.title(x_location,y_location,title_picked)
+            tile_picked=random.randint(1,3)
+            background.tile(x_location,y_location,tile_picked)
 
     ship = stage.Sprite(
         image_bank_sprites, 5, 75, constants.SCREEN_Y -
@@ -44,10 +44,20 @@ def game_scene():
     alien = stage.Sprite(image_bank_sprites, 9, int(
         constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2), 16)
 
+        # create list of lasers for when we shoot
+    lasers = []
+
+    for laser_number in range(constants.TOTAL_NUMBER_OF_LASERS):
+        a_single_laser = stage.Sprite(image_bank_sprites,10,constants.OFF_SCREEN_X,constants.OFF_SCREEN_Y)
+        
+        lasers.append(a_single_laser)
+
+
     # create a stage for the background to show up on
     # and set the frame rate to 60fps
-    game = stage.Stage(ugame.display, constants.FPS)
-    game.layers = [ship] + [alien] + [background]
+    game = stage.Stage(ugame.display, 60)
+    #set the layers,items show up in order
+    game.layers = lasers + [ship] + [alien] + [background]
 
     # render the background and initial location of sprite list
 
@@ -97,17 +107,32 @@ def game_scene():
         # update game logic
         # redraw sprites
         if a_button == constants.button_state["button_just_pressed"]:
+        #fire a laser if we have enough power
+            for laser_number in range(len(lasers)):
+                if lasers[laser_number].x < 0:
+                    lasers[laser_number].move(ship.x, ship.y)
+                    sound.play(pew_sound)
+                    break
 
-            sound.play(pew_sound)
+    # each frame move the lasers, that have been fired up
+    for laser_number in range(len(lasers)):
+        if lasers[laser_number].x > 0:
+            lasers[laser_number].move(lasers[laser_number].x,
+                                    lasers[laser_number].y -
+                                    constants.LASER_SPEED)
+
+        if lasers[laser_number].y < constants.OFF_TOP_SCREEN:
+            lasers[laser_number].move(constants.OFF_SCREEN_X,
+                                    constants.OFF_SCREEN_Y)
+
 
         # redraw sprites
 
-        game.render_sprites([ship] + [alien])
+        game.render_sprites(lasers+[ship] + [alien])
         game.tick()
 
 
-if __name__ == "__main__":
-    splash_scene()
+
 
 
 def splash_scene():
